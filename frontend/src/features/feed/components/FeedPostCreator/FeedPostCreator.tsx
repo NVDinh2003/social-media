6fc0a963eb44e478d400e7df3d80a7cffbe7fd77
Nguyen Van Dinh <nvdinh2003@gmail.com>
2024-08-25 23:02:29 +0700

    fe: custom css FeedPostCreator feature :penguin:

--- frontend/src/features/feed/components/FeedPostCreator/FeedPostCreator.tsx
+++ frontend/src/features/feed/components/FeedPostCreator/FeedPostCreator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import "./FeedPostCreator.css";
 import { Link } from "react-router-dom";
@@ -12,8 +12,26 @@ import ScheduleSVG from "../../../../components/SVGs/ScheduleSVG";
 import LocationSVG from "../../../../components/SVGs/LocationSVG";
 
 export const FeedPostCreator: React.FC = () => {
+  //
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const [active, setActive] = useState<boolean>(false);
+  const [postContent, setPostContent] = useState<string>("");
+
+  const activate = () => {
+    if (!active) setActive(true);
+  };
+
+  const autoGrow = () => {
+    if (textAreaRef && textAreaRef.current) {
+      textAreaRef.current.style.height = "25px";
+      textAreaRef.current.style.height =
+        textAreaRef.current.scrollHeight + "px";
+    }
+  };
+
   return (
-    <div className="feed-post-creator">
+    <div className="feed-post-creator" onClick={activate}>
       <Link to="">
         <img
           src={process.env.REACT_APP_PFP}
@@ -23,7 +41,13 @@ export const FeedPostCreator: React.FC = () => {
       </Link>
 
       <div className="feed-post-creator-right">
-        <div className="feed-post-creator-audience">
+        <div
+          className={
+            active
+              ? "feed-post-creator-audience"
+              : "feed-post-creator-audience hide"
+          }
+        >
           Everyone
           <ExpandMore
             sx={{
@@ -33,14 +57,32 @@ export const FeedPostCreator: React.FC = () => {
         </div>
 
         <textarea
-          className="feed-post-creator-input"
+          className={
+            active
+              ? "feed-post-creator-input input-active"
+              : "feed-post-creator-input"
+          }
           placeholder="What is happening?!"
+          ref={textAreaRef}
+          onChange={autoGrow}
+          cols={50}
+          maxLength={256}
         />
-        <div className="feed-post-creator-reply">
+        <div
+          className={
+            active ? "feed-post-creator-reply" : "feed-post-creator-reply hide"
+          }
+        >
           {<GlobeSVG height={14} width={14} color={"#1DA1F2"} />}
           Everyone can reply
         </div>
-        <div className="feed-post-creator-bottom-icon">
+        <div
+          className={
+            active
+              ? "feed-post-creator-bottom-icon icons-border"
+              : "feed-post-creator-bottom-icon"
+          }
+        >
           <div className="feed-post-creator-icon-bg">
             <MediaSVG height={20} width={20} color={"#1DA1F2"} />
           </div>
@@ -60,12 +102,21 @@ export const FeedPostCreator: React.FC = () => {
             {" "}
             <ScheduleSVG height={20} width={20} color={"#1DA1F2"} />
           </div>
-          <div className="feed-post-creator-icon-bg">
+          <div className="feed-post-creator-location">
             {" "}
             <LocationSVG height={20} width={20} color={"rgba(29,161,242,.5)"} />
           </div>
+          <button
+            className={
+              postContent === ""
+                ? "feed-post-creator-post-button"
+                : "feed-post-creator-post-button post-active"
+            }
+            disabled={postContent === ""}
+          >
+            Post
+          </button>
         </div>
-        <button className="feed-post-creator-post-button">Post</button>
       </div>
     </div>
   );
